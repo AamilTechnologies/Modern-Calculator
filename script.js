f80bfd7a015198b290f8e2896b365f1d473b7f42
Aamil Qureshi <benmyhero10@gmail.com>
2026-05-17 08:55:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -381,17 +381,21 @@ console.log("PWA Ready");
 
 /* Hide Loader */
 
-window.addEventListener(
-"load",
-()=>{
+window.addEventListener("load",()=>{
 
 setTimeout(()=>{
 
-document.getElementById(
-"loader"
-).style.display = "none";
+const loader =
+document.getElementById("loader");
 
-},2500);
+loader.style.opacity = "0";
 
-}
-);
+setTimeout(()=>{
+
+loader.style.display = "none";
+
+},500);
+
+},2000);
+
+});
